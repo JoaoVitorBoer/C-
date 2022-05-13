b81efcfacdb4286dec8f5401a355edf48b702548
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-05-13 17:40:44 -0300

    Aula

--- Demos/DemoHeranca/FiguraGeometrica.cs
+++ Demos/DemoHeranca/FiguraGeometrica.cs
@@ -3,12 +3,12 @@ public abstract class FiguraGeometrica : IComparable<FiguraGeometrica>
     public int X {get;set;}
     public int Y {get;set;}
     public string Id{get;init;}
+    public abstract int Area {get;}
 
-    public abstract int Area {get;}    
     public FiguraGeometrica(int x, int y, string id)
     {
         X = x;
-        Y= y;
+        Y = y;
         Id = id;
     }
 
@@ -19,59 +19,59 @@ public abstract class FiguraGeometrica : IComparable<FiguraGeometrica>
 
     public int CompareTo(FiguraGeometrica? other)
     {
-        if(other == null) return 1;
+        if (other == null) return 1;
         return Area.CompareTo(other.Area);
     }
-    
-    
-    public class ComparadorPorPontoCartesioano : IComparer<FiguraGeometrica> 
+
+    public class ComparadorPorPontoCartesiano : IComparer<FiguraGeometrica>
     {
-      public int Compare(FiguraGeometrica? fig1, FiguraGeometrica? fig2)
-      {
-          if (fig1 == null)
-          { 
-              if(fig2 == null)
-              {
-                  return 0;
-              }
-              else
-              {
-                  return -1;
-              }
-          }
-          else
-          {
-              if(fig2 == null)
-              {
-                  return 1;
-              }
-              else
-              {
-                  //comparar os pontos de cada figura
-                 if (fig1.X < fig2.X)
-                 {
-                     return -1;
-                 }
-                 else if (fig1.X > fig2.X){
-                     return 1;
-                 }
-                 else
-                 {
-                     if(fig1.Y < fig2.Y)
-                     {
-                         return -1;
-                     }
-                     else if (fig1.Y > fig2.Y)
-                     {
-                         return 1;
-                     }
-                     else
-                     {
-                         return 0;
-                     }
-                 }
-              }
-          }
-      }  
+        public int Compare(FiguraGeometrica? fig1, FiguraGeometrica? fig2)
+        {
+            if (fig1 == null)
+            {
+                if (fig2 == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                if (fig2 == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    //comparar os pontos de cada figura
+                    if (fig1.X < fig2.X)
+                    {
+                        return -1;
+                    }
+                    else if (fig1.X > fig2.X)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        if (fig1.Y < fig2.Y)
+                        {
+                            return -1;
+                        }
+                        else if (fig1.Y > fig2.Y)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
